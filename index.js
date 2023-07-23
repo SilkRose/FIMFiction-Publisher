4338d6f3ee493bdedc03c381c52d7478f4254553
Silk Rose <silkrose@love-tolerance.com>
2023-07-23 06:32:55 -0400

    Puppeteer testing

--- index.js
+++ index.js
@@ -3,16 +3,43 @@ const puppeteer = require("puppeteer");
 async function mane() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+
+    await page.goto("https://www.fimfiction.net/");
+
+    const loginLink = (await page.$('input[name="username"]')) !== null;
+
+    if (loginLink) {
+        console.log("User is not logged in.");
+        await login(page);
+    } else {
+        console.log("User is logged in.");
+    }
+
     await page.goto("https://www.fimfiction.net/story/539422/the-pink-tax");
 
-    let likes = await page.evaluate(() => {
-        const likesElement = document.querySelector(".likes");
-        return likesElement.innerText;
-    });
+    let likes = await page.$eval(
+        ".likes",
+        (likesElement) => likesElement.innerText
+    );
 
     console.log(likes + " likes on The Pink Tax.");
 
     await browser.close();
+}
+
+async function login(page) {
+    await page.focus('input[name="username"]');
+
+    await page.type('input[name="username"]', "Test");
+
+    await page.waitForTimeout(3000);
+
+    const enteredUsername = await page.$eval(
+        'input[name="username"]',
+        (input) => input.value
+    );
+
+    console.log("Entered Username:", enteredUsername);
 }
 
 mane();
