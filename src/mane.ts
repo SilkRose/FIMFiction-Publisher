5afe947d5605f7638ec5d2acd922bd61ca75a238
Silk Rose <silkrose@love-tolerance.com>
2023-07-27 01:17:06 -0400

    Get basic login working

--- src/mane.ts
+++ src/mane.ts
@@ -1,18 +1,22 @@
 import puppeteer from "puppeteer";
+import readlineSync from 'readline-sync';
 
 async function mane() {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 100
+    });
     const page = await browser.newPage();
 
     await page.goto("https://www.fimfiction.net/");
 
-    const loginLink = (await page.$('input[name="username"]')) !== null;
+    const logged_in = (await page.$('input[name="username"]')) == null;
 
-    if (loginLink) {
+    if (logged_in) {
+        console.log("User is logged in.");
+    } else {
         console.log("User is not logged in.");
         await login(page);
-    } else {
-        console.log("User is logged in.");
     }
 
     await page.goto("https://www.fimfiction.net/story/539422/the-pink-tax");
@@ -28,18 +32,25 @@ async function mane() {
 }
 
 async function login(page: any) {
+    const login_button = await page.$x("//button[contains(., 'Log In')]");
     await page.focus('input[name="username"]');
-
-    await page.type('input[name="username"]', "Test");
-
-    await page.waitForTimeout(3000);
-
-    const enteredUsername = await page.$eval(
-        'input[name="username"]',
-        (input: { value: any; }) => input.value
-    );
-
-    console.log("Entered Username:", enteredUsername);
+    await page.type('input[name="username"]', input_username());
+    await page.waitForTimeout(1000);
+    await page.focus('input[name="password"]');
+    await page.type('input[name="password"]', input_password());
+    await page.waitForTimeout(1000);
+    await login_button[0].click();
+    await page.waitForTimeout(4000);
 }
+
+function input_username() {
+    return readlineSync.question('Enter your username or email: ');
+  }
+
+function input_password() {
+    return readlineSync.question('Enter your password (hidden input): ', {
+      hideEchoBack: true,
+    });
+  }
 
 mane();
