085dc35e899c549f7c474a2a7c93e546cc8b8324
Silk Rose <silkrose@love-tolerance.com>
2023-07-28 21:51:01 -0400

    Work on collecting published status

--- src/mane.ts
+++ src/mane.ts
@@ -3,10 +3,22 @@ import readlineSync from "readline-sync";
 import { promises as fsPromises } from "fs";
 import fs from "fs";
 
+const submit_selector =
+    '<a data-click="submit"><i class="fa fa-cloud-upload"></i> Submit</a>';
+const publish_chapter_selector =
+    '<span class="chapter_publish"><i class="fa fa-upload"></i> Publish</span>';
+const unpublished_chapter_selector =
+    '<i class="fa fa-circle" title="Not Published" style="color:red;"></i>';
+const revoke_submission_selector =
+    '<a class="button-revoke-story" data-click="revoke"><i class="fa fa-reply"></i> Revoke Submission </a>';
+const popout_confirm_selector =
+    '<button id="ok_button" class="styled_button"><i class="fa fa-check"></i> Ok</button>';
+const popout_cancel_selector =
+    '<button id="cancel_button" class="styled_button styled_button_red"><i class="fa fa-times"></i>  Cancel</button>';
+
 async function mane() {
     const browser = await puppeteer.launch({
-        headless: true,
-        slowMo: 0,
+        headless: "new",
     });
     const page = await browser.newPage();
 
@@ -81,6 +93,15 @@ async function get_story_data(page: Page) {
             });
         return stories;
     });
+    for (let story of stories) {
+        await page.goto("https://www.fimfiction.net/story/" + story.id, {
+            waitUntil: "networkidle2",
+        });
+        const published = await page
+            .$eval(submit_selector, () => false)
+            .catch(() => true);
+        console.log(published, story.name);
+    }
     return stories;
 }
 
